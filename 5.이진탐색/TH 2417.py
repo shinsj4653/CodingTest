f77ff864c8a915359e9b735abefe0ce82d4901d4
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-03-30 22:10:10 +0900

    3/30

---
+++ 5.이진탐색/TH 2417.py
@@ -0,0 +1,13 @@
+n= int(input())
+# 시작 : 0, 끝: n
+start = 0
+end = n
+ans=0
+while start<=end:
+    mid=(start+end)//2
+    if mid*mid<n:
+        start=mid+1
+    elif mid*mid>=n:
+        end=mid-1
+# print(start,end,end='       ')
+print(start)
